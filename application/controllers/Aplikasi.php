4969e9b015dd4daaec9bbb706ff6247f5f5961d9
mzaini30 <muhzaini30@gmail.com>
2019-11-06 14:42:51 +0800

    oke

--- application/controllers/Aplikasi.php
+++ application/controllers/Aplikasi.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Aplikasi extends MY_Controller {
 	public function index(){
-		$this->twig->display('aplikasi/beranda');
+		$data = $this->db->order_by('nama')->get('aplikasi')->result();
+		$this->twig->display('aplikasi/beranda', compact('data'));
 	}
 }
